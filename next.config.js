85a293482b40b376c6dfa1fa0cab22dc4fbf8c15
miravicson <victorughonu95@gmail.com>
2021-10-17 19:19:50 +0100

    trying to get build to work

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 const withImages = require('next-images');
 const withMDX = require('@next/mdx')();
-module.exports = withMDX(
-  withImages({
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
-    target: 'serverless',
-    esModule: true,
-    reactStrictMode: true,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      console.log(JSON.stringify(defaultLoaders, null, 2));
-      if (isServer) {
-        require('./utils/generateSiteMap');
-      }
-      return config;
-    },
-  })
-);
+// module.exports = withMDX(
+//   withImages({
+//     sassOptions: {
+//       includePaths: [path.join(__dirname, 'styles')],
+//     },
+//     target: 'serverless',
+//     esModule: true,
+//     reactStrictMode: true,
+//     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//       console.log(JSON.stringify(defaultLoaders, null, 2));
+//       if (isServer) {
+//         require('./utils/generateSiteMap');
+//       }
+//       return config;
+//     },
+//   })
+// );
 // module.exports = withImages(
 //   withMDX({
 //     sassOptions: {
@@ -34,4 +34,4 @@ module.exports = withMDX(
 //   })
 // );
 
-// module.exports = withMDX();
+module.exports = withMDX();
