b91fa011ba01748c17e9216e75d0f7adabbaf7c7
miravicson <victorughonu95@gmail.com>
2021-10-17 19:06:55 +0100

    before building

--- next.config.js
+++ next.config.js
@@ -1,5 +1,37 @@
-module.exports = {
-  // Supported targets are "serverless" and "experimental-serverless-trace"
-  target: "serverless",
-  generateBuildId: () => "build"
-}
+const path = require('path');
+const withImages = require('next-images');
+const withMDX = require('@next/mdx')();
+module.exports = withMDX(
+  withImages({
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+    target: 'serverless',
+    esModule: true,
+    reactStrictMode: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      console.log(JSON.stringify(defaultLoaders, null, 2));
+      if (isServer) {
+        require('./utils/generateSiteMap');
+      }
+      return config;
+    },
+  })
+);
+// module.exports = withImages(
+//   withMDX({
+//     sassOptions: {
+//       includePaths: [path.join(__dirname, 'styles')],
+//     },
+//     target: 'serverless',
+//     esModule: true,
+//     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//       if (isServer) {
+//         require('./utils/generateSiteMap');
+//       }
+//       return config;
+//     },
+//   })
+// );
+
+// module.exports = withMDX();
